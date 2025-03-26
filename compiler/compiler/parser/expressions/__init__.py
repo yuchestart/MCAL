e614146be247c37391bd50fb81b5d33074b3a16c
Che Yu <99137611+yuchestart@users.noreply.github.com>
2025-03-25 20:49:40 -0500

    Create compiler.parser.expressions

--- compiler/compiler/parser/expressions/__init__.py
+++ compiler/compiler/parser/expressions/__init__.py
@@ -3,10 +3,14 @@ from compiler.parser.expressions.base import ParserExpressionsBase
 
 from compiler.parser.expressions.datatypes import ParserDataTypes
 from compiler.parser.expressions.literals import ParserExpressionsLiterals
+from compiler.parser.expressions.calculations import ParserExpressionsCalculations
 
-from compiler.ast.base import BaseNode
+from compiler.ast.values import Expression
 
 from compiler.tokenizer.interfaces import Token
+
+import sys
+sys.setrecursionlimit(50)
 
 import pcre2
 from typing import *
@@ -14,19 +18,38 @@ from typing import *
 
 class ParserExpressions(
     ParserExpressionsLiterals,
+    ParserExpressionsCalculations,
     ParserDataTypes,
     ParserExpressionsBase,
     ParserBase
 ):
+    def parse_expression_atom(self, tokens:List[Token]) -> Tuple[int, Expression]:
+        parsers = [
+            self.parse_expression_group,
+            self.parse_expression_literal,
+            self.parse_expression_assignment,
+        ]
+        ret: Expression | None = None
+        finalpos:int = 0
+        for parser in parsers:
+            print(parser)
+            pos, node = parser(tokens)
+            if node is not None:
+                ret = node
+                finalpos = pos
+                break
+        
+        return finalpos,ret
 
     # Here it gets overridden
-    def parse_expression(self, tokens:List[Token]) -> Tuple[int, BaseNode]:
-        expressionparsers = [
-            self.parse_literal
+    def parse_expression(self, tokens:List[Token]) -> Tuple[int, Expression]:
+        parsers = [
+            self.parse_expression_opchain,
+            self.parse_expression_atom
         ]
-        ret: BaseNode | None = None
+        ret: Expression | None = None
         finalpos:int = 0
-        for parser in expressionparsers:
+        for parser in parsers:
             pos, node = parser(tokens)
             if node is not None:
                 ret = node
