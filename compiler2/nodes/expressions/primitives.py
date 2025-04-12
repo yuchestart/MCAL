336eef7893422468e59b56ddfa8e39ee7b96fdbc
Che Yu <99137611+yuchestart@users.noreply.github.com>
2025-04-11 19:52:43 -0500

    Added primitive-related AST nodes

--- compiler2/nodes/expressions/primitives.py
+++ compiler2/nodes/expressions/primitives.py
@@ -11,3 +11,22 @@ class Number:
 class String:
     value:str
     substitutes:dict[tuple[int,int],Any]
+
+@dataclass
+class Boolean:
+    value:bool
+
+@dataclass
+class Null:
+    pass
+
+@dataclass
+class UUID:
+    value:list[int]
+
+@dataclass
+class Coordinate:
+    type:str
+    x:Any
+    y:Any
+    z:Any
